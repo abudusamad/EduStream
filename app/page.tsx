266dad049f505c75652898c412f93114f7e2f5b9
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-10-17 22:37:27 +0000

    Adding the button to the component which can be used as a global variable

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
-import Image from 'next/image'
+import { Button } from "@/components/ui/button";
+
+
 
 export default function Home() {
   return (
-    <p className='text-3xl font-medium text-slate-300 '>Hello world</p>
+    <div>
+      <p className='text-3xl text-medium text-sky-700'>
+        Hello world
+      </p>
+      <Button variant="destructive">
+        Click me
+      </Button>
+    
+   </div>
   )
 }
