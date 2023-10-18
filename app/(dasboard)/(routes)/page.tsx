dd8e1cd9a7ae8a929ab8c649cb192677a310ead2
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-10-18 20:21:51 +0000

    working on the navbar of the components

---
+++ app/(dasboard)/(routes)/page.tsx
@@ -0,0 +1,13 @@
+// import { UserButton } from "@clerk/nextjs";
+
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button>
+        click me
+      </Button>
+   </div>
+  )
+}
