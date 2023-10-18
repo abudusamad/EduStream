49e7346c88ecae1bff2719337f24562f0da476f8
abudusamad <abudusamed@gmail.com>
2023-10-18 16:29:53 +0000

    working on  the sidebar for the page

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export const Logo = () => {
     return (
         <Image
-            height={130}
-            width={130}
+            height={40}
+            width={40}
             alt="logo"
             src="/logo.svg"
         />
--- app/(dashboard)/_components/sidebar-routes.tsx
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -1,6 +1,24 @@
 "use client";
+import { Compass, Layout } from "lucide-react";
+import { usePathname } from "next/navigation";
+
+
+
+const guestRoutes = [
+    {
+
+        icon: Layout,
+        label: "Dashboard",
+        href: "/"
+    }, {
+        icon: Compass,
+        label: "Browse",
+        href:"/search",
+    },
+];
 
 export const SidebarRoutes = () => {
+    const pathname = usePathname();
     return (
         <div>
             Routes
