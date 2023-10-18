520601e5221c8461ecc08270115d684b89a54411
abudusamad <abudusamed@gmail.com>
2023-10-18 13:33:19 +0000

    Adding sigin and signup for the platform

--- app/(dasboard)/_conponents/sidebar.tsx
+++
@@ -1,7 +0,0 @@
-export const Sidebar = ()=> {
-    return(
-        <div className="h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm">
-             Sidebar!
-        </div>
-    )
-}
