76051e0144ccfcbdb42c40c801164b58501227ed
abudusamad <abudusamed@gmail.com>
2023-10-18 17:43:02 +0000

    Finsished with the sidebar of the navigation

--- app/(dashboard)/_components/sidebar-routes.tsx
+++ app/(dashboard)/_components/sidebar-routes.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Compass, Layout } from "lucide-react";
+import { BarChart, Compass, Layout, List,History } from "lucide-react";
 import { usePathname } from "next/navigation";
 import { SidebarItem } from "./sidebar-items";
 
@@ -16,14 +16,44 @@ const guestRoutes = [
         label: "Browse",
         href:"/search",
     },
+    {
+        icon: History,
+        label: "History",
+        href:"/history"
+    }
 ];
+
+const teacherRoutes = [
+    {
+        icon: List,
+        label: "Courses",
+        href:"/teacher/courses",
+    }, 
+    {
+        icon: BarChart,
+        label: "Analytics",
+        href:"teacher/analytics",
+    }
+]
+
+
 
 export const SidebarRoutes = () => {
     const pathname = usePathname();
+    const isTeacherPage = pathname?.includes("/teacher");
+    const routes = isTeacherPage ? teacherRoutes : guestRoutes;
 
     return (
         <div className="flex flex-col w-full">
-            <SidebarItem/>
-        </div>
-    )
+            {routes.map((route) => (
+                <SidebarItem
+                    key={route.href}
+                    icon={route.icon}
+                    label={route.label}
+                    href={route.href}
+                />
+            ))}
+
+			</div>
+		);
 }
