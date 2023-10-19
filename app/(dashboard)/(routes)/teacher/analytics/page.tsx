8d531690515ac2886d1a9bb8a5cdd9855cd4fdb0
abudusamad <abudusamed@gmail.com>
2023-10-19 13:26:18 +0000

    working on the form control for the courses that is being registers

--- app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -1,11 +1,7 @@
 const AnalyticsPage = () => {
-    return (<div>
-<<<<<<< HEAD
-        This is Analytics Page
-=======
-        This is an Analytics Page
->>>>>>> dd2e269 (Finished with the courses and the anlytics page)
-    </div> );
+    return (<div className="p-6">
+        Analytic Page
+    </div>)
 }
  
 export default AnalyticsPage;
--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -1,11 +1,45 @@
 "use client";
 
 import * as z from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { useRouter } from "next/router"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormLabel,
+    FormMessage
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
+const formSchema = z.object({
+    title: z.string().min(1, {
+        message: "Title is required",
+    })
+})
 
 const CreatePage = () => {
-    return (<div>
-        Create Page
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            title:""
+        }
+    })
+    const { isSubmitting, isValid } = form.formState
+    
+    const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values)
+    }
+    return (<div className="max-w-5xl mx-auto flex md:items-center md:justify-center h-full p-6">
+        <div>
+            <h1 className="text-2xl">
+                Name your course
+            </h1>
+        </div>
     </div> );
 }
  
