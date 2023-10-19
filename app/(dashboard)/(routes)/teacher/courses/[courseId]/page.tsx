e469c3beb02f6f12ae0638f8eb78d631a334b4d0
abudusamad <abudusamed@gmail.com>
2023-10-19 19:24:52 +0000

    Working on the file upload library for the api

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -5,6 +5,7 @@ import { redirect } from "next/navigation";
 import { IconBadge } from "@/components/icon-badge";
 import { LayoutDashboard } from "lucide-react";
 import { TitleForm } from "./_components/title-form";
+import { DescriptionForm } from "./_components/description-form";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
 	const { userId } = auth();
@@ -43,14 +44,12 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
 			</div>
 			<div className="gird grid-cols-1 md:grid-cols02 gap-6 mt-16">
 				<div>
-                    <div className="flex items-center gap-x-2">
-                        <IconBadge icon={LayoutDashboard  } />
+					<div className="flex items-center gap-x-2">
+						<IconBadge icon={LayoutDashboard} />
 						<h2 className="text-xl">Customize your course</h2>
-                    </div>
-                    <TitleForm
-                        initialData={course}
-                        courseId={course.id}
-                    />
+					</div>
+					<TitleForm initialData={course} courseId={course.id} />
+					<DescriptionForm initialData={course} courseId={course.id} />
 				</div>
 			</div>
 		</div>
