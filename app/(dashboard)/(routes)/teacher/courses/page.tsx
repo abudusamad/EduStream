1958bdd19c8ab787ba0673188a8c8b4d5003080c
abudusamad <abudusamed@gmail.com>
2023-10-19 12:53:57 +0000

    Finished with the teacher page

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,23 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { Button } from "@/components/ui/button";
-import { Link } from "lucide-react";
 
-const CoursesPage = () => {
-    return (<div>
-        <Link href="/teacher/courses/">
-        <Button className="p-6">
-            Courses
-       </Button>
-        </Link>
-=======
-const CoursesPage = () => {
-    return (<div>
-        This is a course page
->>>>>>> dd2e269 (Finished with the courses and the anlytics page)
-    </div> );
-}
-=======
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
@@ -25,11 +6,11 @@ const CoursesPage = () => {
 	return (
 		<div className="p-6">
 			<Link href="/teacher/create">
-				<Button variant="destructive">New Course</Button>
+				<Button variant="default">New Course</Button>
 			</Link>
 		</div>
 	);
 };
->>>>>>> 228026d (Adding forms and inputs to the page for creating results)
+
  
 export default CoursesPage;
--- components/navbar-route.tsx
+++ components/navbar-route.tsx
@@ -1,11 +1,12 @@
 "use client";
 
 import { UserButton } from "@clerk/nextjs";
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { Link, LogOut } from "lucide-react";
+import { LogOut} from "lucide-react";
 import { usePathname } from "next/navigation";
 import { Button } from "./ui/button";
+
+import Link from "next/link"
+
 
 export const NavbarRoutes = () => {
 	const pathname = usePathname();
@@ -32,45 +33,4 @@ export const NavbarRoutes = () => {
 		</div>
 	);
 };
-=======
 
-export const NavbarRoutes = () => {
-    return (<div className="flex gap-x-2 ml-auto">
-        <UserButton/>
-    
-</div>)
-}
->>>>>>> bd4e98f (working on the navbar)
-=======
-import { LogOut } from "lucide-react";
-import { usePathname } from "next/navigation";
-import { Button } from "./ui/button";
-import Link from "next/link";
-
-export const NavbarRoutes = () => {
-	const pathname = usePathname();
-
-	const isTeacherPage = pathname?.startsWith("/teacher");
-	const isPlayerPage = pathname?.includes("/chapter");
-
-	return (
-		<div className="flex gap-x-2 ml-auto">
-			{isTeacherPage || isPlayerPage ? (
-				<Link href="/">
-					<Button size="sm" variant="ghost">
-						<LogOut className="w-4 h-4 mr-2" />
-						Exit
-					</Button>
-				</Link>
-			) : (
-				<Link href="/teacher/courses">
-					<Button size="sm" variant="ghost">
-						Teacher mode
-					</Button>
-				</Link>
-			)}
-			<UserButton />
-		</div>
-	);
-};
->>>>>>> dd2e269 (Finished with the courses and the anlytics page)
