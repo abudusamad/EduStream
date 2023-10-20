343efa9c770896a1d7b0b7752fa88161dd376f7d
abudusamad <abudusamed@gmail.com>
2023-10-20 18:07:07 +0000

    Finished with the chapters

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
@@ -20,6 +20,7 @@ import {
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { Input } from "@/components/ui/input";
+import { ChaptersList } from "./chapters-list";
 
 
 interface ChaptersFormProps {
@@ -137,6 +138,11 @@ export const ChaptersForm = ({ initialData, courseId }: ChaptersFormProps) => {
 					)}
 				>
 					{!initialData.chapters.length && "No chapters"}
+					 <ChaptersList
+            onEdit={onEdit}
+            onReorder={onReorder}
+            items={initialData.chapters || []}
+          />
 				</div>
 			)}
 			{!isCreating && (
