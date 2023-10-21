63a56203ef91fe1011f789656676a13ee7dbf55b
abudusamad <abudusamed@gmail.com>
2023-10-21 13:53:09 +0000

    working on the chaters and reorder of the chapter page

---
+++ app/api/courses/[courseId]/chapters/reorder/route.ts
@@ -0,0 +1,38 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function PUT(
+	req: Request,
+	{ params }: { params: { courseId: string } }
+) {
+	try {
+        const { userId } = auth();
+        const { list } = await req.json();
+
+		if (!userId) {
+			return new NextResponse("Unauthorized", { status: 401 });
+		}
+
+		const courseOwener = await db.course.findUnique({
+			where: {
+				id: params.courseId,
+				userId: userId,
+			},
+		});
+
+		if (!courseOwener) {
+			return new NextResponse("Unauthorized", { status: 401 });
+        }
+        for (let item of list) {
+            await db.chapter.update({
+                where: { id: item.id },
+                data: {position: item.Position}
+            })
+        }
+        return new NextResponse("Success",{status: 200})
+	} catch (error) {
+		console.log("[REORDERED]", error);
+		return new NextResponse("Internal Error", { status: 500 });
+	}
+}
