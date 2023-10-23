9a111185c582fbe16a4b6b3ef5ec164a86a0686a
abudusamad <abudusamed@gmail.com>
2023-10-23 09:41:27 +0000

    modifed the database for the prisma client and generate

--- lib/db.ts
+++ lib/db.ts
@@ -7,3 +7,15 @@ declare global {
 export const db = globalThis.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+
+const prisma = new PrismaClient();
+
+(async () => {
+	try {
+		console.log(await prisma.widget.create({ data: {} }));
+	} catch (err) {
+		console.error("error executing query:", err);
+	} finally {
+		prisma.$disconnect();
+	}
+})();
