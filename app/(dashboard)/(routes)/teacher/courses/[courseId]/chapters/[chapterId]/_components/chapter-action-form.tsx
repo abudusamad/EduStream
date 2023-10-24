a751de5b79f3bf5ee50082137df9d1315916d21c
abudusamad <abudusamed@gmail.com>
2023-10-24 14:20:30 +0000

    Finished with the confirm modal of the action button

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-action-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-action-form.tsx
@@ -1,37 +1,39 @@
 "use client";
 
+import { ConfirmModal } from "@/components/modals/confirm-modal";
 import { Button } from "@/components/ui/button";
 import { Trash } from "lucide-react";
 import { useState } from "react";
 
-interface ChapterActionFormProps{
-    disabled: boolean;
-    courseId: string;
-    chapterId: string;
-    isPublished: boolean;
+interface ChapterActionFormProps {
+	disabled: boolean;
+	courseId: string;
+	chapterId: string;
+	isPublished: boolean;
 }
-
 
 export const ChapterActionForm = ({
-    disabled,
-    courseId,
-    chapterId,
-    isPublished,
+	disabled,
+	courseId,
+	chapterId,
+	isPublished,
 }: ChapterActionFormProps) => {
-    const [isLoading, setIsLoading] = useState(false);
-    return (
-        <div className="flex items-center gap-x-2">
-            <Button
-                onClick={()=>{}}
-                disabled={disabled || isLoading}
-                variant="outline"
-                size="sm"
-            >
-                {isPublished ? "Unpublish" : "Publish"}
-            </Button>
-            <Button size="sm" disabled={isLoading}>
-                <Trash className="w-4 h-4 mr-2"/>
-            </Button>
-        </div>
-    )
-}
+	const [isLoading, setIsLoading] = useState(false);
+	return (
+		<div className="flex items-center gap-x-2">
+			<Button
+				onClick={() => {}}
+				disabled={disabled || isLoading}
+				variant="outline"
+				size="sm"
+			>
+				{isPublished ? "Unpublish" : "Publish"}
+			</Button>
+			<ConfirmModal onConfirm={() => {}}>
+				<Button size="sm" disabled={isLoading}>
+					<Trash className="w-4 h-4 mr-2" />
+				</Button>
+			</ConfirmModal>
+		</div>
+	);
+};
