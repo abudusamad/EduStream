1e6c3a6e0eb0c63d457126a03e2df269d1d8487b
abudusamad <abudusamed@gmail.com>
2023-10-24 18:19:50 +0000

    Finsihed with the navbar

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,11 +1,11 @@
-import { NavbarRoute } from "@/components/navbar-route"
+import { NavbarRoutes } from "@/components/navbar-route"
 import { MobileSidebar } from "./mobile-sideabr"
 
 export const Navbar = () => {
     return (
         <div className="p-4 border-b h-full shadow-sm bg-white flex items-center ">
             <MobileSidebar />
-            <NavbarRoute/>
+            <NavbarRoutes/>
 
         </div>
     )
