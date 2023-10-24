9cc381e977c6fa1acc674ad42d9f9d54abeab7a7
abudusamad <abudusamed@gmail.com>
2023-10-24 18:13:34 +0000

    Finished with the route of the ToastProvider

--- components/providers/toaster-provider.tsx
+++ components/providers/toaster-provider.tsx
@@ -1,15 +1,6 @@
 "use client";
-<<<<<<< HEAD
-import { Toaster } from "react-hot-toast";
-
-export const ToastProvider = () => {
-	return <Toaster />;
-};
-=======
-
 import {Toaster} from "react-hot-toast"
 
 export const ToastProvider = () => {
     return <Toaster/>
 }
->>>>>>> 615bf4af3c9c9908287fdde42ccc75cacc4d50bb
