828cb907a5843856d2180d4524b1846c58add6be
abudusamad <abudusamed@gmail.com>
2023-10-25 11:03:10 +0000

    Working on the api for updating and deleting a course videos

--- app/api/courses/[courseId]/route.ts
+++ app/api/courses/[courseId]/route.ts
@@ -1,15 +1,12 @@
-<<<<<<< HEAD
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
+import Mux from "@mux/mux-node";
 import { NextResponse } from "next/server";
 
-=======
-import { auth } from "@clerk/nextjs";
-import { NextResponse } from "next/server";
-
-import { db } from "@/lib/db";
-
-
+const { Video } = new Mux(
+	process.env.MUX_TOKEN_ID!,
+	process.env.MUX_TOKEN_SECRET!
+);
 
 export async function DELETE(
 	req: Request,
@@ -17,11 +14,9 @@ export async function DELETE(
 ) {
 	try {
 		const { userId } = auth();
-
 		if (!userId) {
 			return new NextResponse("Unauthorized", { status: 401 });
 		}
-
 		const course = await db.course.findUnique({
 			where: {
 				id: params.courseId,
@@ -35,60 +30,48 @@ export async function DELETE(
 				},
 			},
 		});
-
 		if (!course) {
-			return new NextResponse("Not found", { status: 404 });
+			return new NextResponse("Not Found", { status: 404 });
 		}
-
-		
-
-		const deletedCourse = await db.course.delete({
+		for (const chapter of course.chapters) {
+			if (chapter.muxData?.assetId) {
+				await Video.Assets.del(chapter.muxData.assetId);
+			}
+		}
+		const deleteCourse = await db.course.delete({
 			where: {
 				id: params.courseId,
 			},
 		});
-
-		return NextResponse.json(deletedCourse);
+		return NextResponse.json(deleteCourse);
 	} catch (error) {
-		console.log("[COURSE_ID_DELETE]", error);
-		return new NextResponse("Internal Error", { status: 500 });
+		console.log("[COURSE_ID]", error);
+		return new NextResponse("Internal Server Error", { status: 500 });
 	}
 }
 
->>>>>>> 615bf4af3c9c9908287fdde42ccc75cacc4d50bb
 export async function PATCH(
 	req: Request,
 	{ params }: { params: { courseId: string } }
 ) {
 	try {
 		const { userId } = auth();
-<<<<<<< HEAD
-=======
-		const { courseId } = params;
->>>>>>> 615bf4af3c9c9908287fdde42ccc75cacc4d50bb
 		const values = await req.json();
-
 		if (!userId) {
 			return new NextResponse("Unauthorized", { status: 401 });
 		}
-
 		const course = await db.course.update({
 			where: {
-<<<<<<< HEAD
 				id: params.courseId,
-=======
-				id: courseId,
->>>>>>> 615bf4af3c9c9908287fdde42ccc75cacc4d50bb
 				userId,
 			},
 			data: {
 				...values,
 			},
 		});
-
 		return NextResponse.json(course);
 	} catch (error) {
 		console.log("[COURSE_ID]", error);
-		return new NextResponse("Internal Error", { status: 500 });
+		return new NextResponse("Internal Server Error", { status: 500 });
 	}
 }
