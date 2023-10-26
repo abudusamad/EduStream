6c52c778d51b82489b0cc88b533da40daf2b231f
abudusamad <abudusamed@gmail.com>
2023-10-26 16:54:23 +0000

    finished with  teh api routes and the pages

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -36,7 +36,7 @@ const CreatePage = () => {
         try {
             const response = await axios.post("/api/courses", values);
             router.push(`/teacher/courses/${response.data.id}`);
-            console.log("Course created successfully");
+            toast.success("Course created");
         } catch {
             toast.error("Something went wrong");
         }
