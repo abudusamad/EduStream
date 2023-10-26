83712d3437817ec0738f086f9b1e2ddb82f03f2e
abudusamad <abudusamed@gmail.com>
2023-10-26 17:26:51 +0000

    Adding icon Badge to the page

---
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -0,0 +1,72 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
+	const { userId } = auth();
+	if (!userId) {
+		return redirect("/");
+	}
+	const course = await db.course.findUnique({
+		where: {
+			id: params.courseId,
+			userId,
+		},
+		include: {
+			chapters: {
+				orderBy: {
+					position: "asc",
+				},
+			},
+			attachments: {
+				orderBy: {
+					createdAt: "desc",
+				},
+			},
+		},
+	});
+
+	const categories = await db.category.findMany({
+		orderBy: {
+			name: "asc",
+		},
+	});
+	if (!course) {
+		return redirect("/");
+	}
+	const requiredFields = [
+		course.title,
+		course.description,
+		course.price,
+		course.categoryId,
+		course.imageUrl,
+		course.chapters.some((chapter) => chapter.isPublished),
+	];
+
+	const totalFields = requiredFields.length;
+	const completedFields = requiredFields.filter(Boolean).length;
+
+	const completionText = `${completedFields}/${totalFields} fields completed`;
+	const isComplete = requiredFields.every(Boolean);
+
+	return (
+		<div className="p-6">
+			<div className="flex items-center justify-between">
+				<div className="flex flex-col gap-y-2">
+					<h1 className="text-2xl font-medium">Course setup</h1>
+					<span>{completionText}</span>
+				</div>
+            </div>
+            <div className="grid grid-col-1 md:grid-col-2 gap-6 mt-16">
+                <div>
+                    <div className="flex items-center gap-x-2">
+                        
+                    </div>
+                </div>
+
+            </div>
+		</div>
+	);
+};
+
+export default CourseIdPage;
