6b99aacac90c32a59d61e3379f01e77a0b5c9047
abudusamad <abudusamed@gmail.com>
2023-10-26 18:17:38 +0000

    Adding the sidebar sheet for small screens and mobile versions

--- app/(dashboard)/_components/mobile-sidebar.tsx
+++ app/(dashboard)/_components/mobile-sidebar.tsx
@@ -1,18 +1,17 @@
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Menu } from "lucide-react";
+
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import Sidebar from "./sidebar";
 
 export const MobileSidebar = () => {
 	return (
-		<div>
-			<Sheet>
-				<SheetTrigger className="md: hidden pr-4 hover:opacity-75 transition ">
-					<Menu />
-				</SheetTrigger>
-				<SheetContent side="left" className="p-0 bg-white">
-					<Sidebar />
-				</SheetContent>
-			</Sheet>
-		</div>
+		<Sheet>
+			<SheetTrigger className="md:hidden pr-4 hover:opacity-75 transition">
+				<Menu />
+			</SheetTrigger>
+			<SheetContent side="left" className="p-0 bg-white">
+				<Sidebar/>
+			</SheetContent>
+		</Sheet>
 	);
 };
