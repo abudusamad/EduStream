2d24ca9a22ab90dbf47b8b1886f1cabeb9c8dfbd
abudusamad <abudusamed@gmail.com>
2023-10-30 17:27:44 +0000

    working on the  categories form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/category-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/category-form.tsx
@@ -5,6 +5,9 @@ import axios from "axios";
 import { zodResolver } from "@hookform/resolvers/zod";
 import toast from "react-hot-toast";
 import { Course } from "@prisma/client";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
 
 interface CategoryFormProps {
     initialData: Course;
@@ -12,11 +15,25 @@ interface CategoryFormProps {
     options: { label: string;  value: string }[];
 }
 
+const formSchema = z.object({
+    categoryId: z.string().min(1,),
+});
+
+
 export const CategoryForm = ({
     initialData,
     courseId,
     options,
-}:CategoryFormProps) => {
+}: CategoryFormProps) => {
+    
+    const [isEditing, setIsEditing] = useState(false);
+    const toggleEdit = () => setIsEditing((current) => !current);
+
+    const router = useRouter();
+
+    const  form = useForm
+
+    
     return (
         <div>
             CategoryForm
