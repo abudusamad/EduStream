2dad3314d80c351918b0420c304fd951e52f5ebc
abudusamad <abudusamed@gmail.com>
2023-11-01 11:24:46 +0000

    making the price for the component of the card

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/price-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/price-form.tsx
@@ -1,27 +1,41 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Course } from "@prisma/client";
-import axios from "axios";
-import { Pencil } from "lucide-react";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
-import { useForm } from "react-hook-form";
-import toast from "react-hot-toast";
 import * as z from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Pencil } from "lucide-react";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { Course } from "@prisma/client";
+
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Input } from "@/components/ui/input";
+import { formatPrice } from "@/lib/format";
 
 interface PriceFormProps {
 	initialData: Course;
 	courseId: string;
 }
+
 const formSchema = z.object({
 	price: z.coerce.number(),
 });
 
 export const PriceForm = ({ initialData, courseId }: PriceFormProps) => {
 	const [isEditing, setIsEditing] = useState(false);
-	const toggleEditing = () => setIsEditing((current) => !current);
+
+	const toggleEdit = () => setIsEditing((current) => !current);
+
 	const router = useRouter();
 
 	const form = useForm<z.infer<typeof formSchema>>({
@@ -30,13 +44,16 @@ export const PriceForm = ({ initialData, courseId }: PriceFormProps) => {
 			price: initialData?.price || undefined,
 		},
 	});
+
 	const { isSubmitting, isValid } = form.formState;
+
 	const onSubmit = async (values: z.infer<typeof formSchema>) => {
 		try {
 			await axios.patch(`/api/courses/${courseId}`, values);
-			toast.success("Price updated successfully");
-			toggleEditing();
-		} catch (error) {
+			toast.success("Course updated");
+			toggleEdit();
+			router.refresh();
+		} catch {
 			toast.error("Something went wrong");
 		}
 	};
@@ -44,23 +61,60 @@ export const PriceForm = ({ initialData, courseId }: PriceFormProps) => {
 	return (
 		<div className="mt-6 border bg-slate-100 rounded-md p-4">
 			<div className="font-medium flex items-center justify-between">
-				Course Price
-				<Button onClick={toggleEditing} variant="ghost">
+				Course price
+				<Button onClick={toggleEdit} variant="ghost">
 					{isEditing ? (
 						<>Cancel</>
 					) : (
 						<>
-							<Pencil className="h-4 w-4 mr-2"/>
-							Edit Price
+							<Pencil className="h-4 w-4 mr-2" />
+							Edit price
 						</>
 					)}
 				</Button>
-            </div>
-            {
-                !isEditing && (
-                    <p
-                )
-            }
+			</div>
+			{!isEditing && (
+				<p
+					className={cn(
+						"text-sm mt-2",
+						!initialData.price && "text-slate-500 italic"
+					)}
+				>
+					{initialData.price ? formatPrice(initialData.price) : "No price"}
+				</p>
+			)}
+			{isEditing && (
+				<Form {...form}>
+					<form
+						onSubmit={form.handleSubmit(onSubmit)}
+						className="space-y-4 mt-4"
+					>
+						<FormField
+							control={form.control}
+							name="price"
+							render={({ field }) => (
+								<FormItem>
+									<FormControl>
+										<Input
+											type="number"
+											step="0.01"
+											disabled={isSubmitting}
+											placeholder="Set a price for your course"
+											{...field}
+										/>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<div className="flex items-center gap-x-2">
+							<Button disabled={!isValid || isSubmitting} type="submit">
+								Save
+							</Button>
+						</div>
+					</form>
+				</Form>
+			)}
 		</div>
 	);
 };
