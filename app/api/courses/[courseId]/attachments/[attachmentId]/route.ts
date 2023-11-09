4f0fad1598f7537db63f32c7aedcf902cda397f4
abudusamad <abudusamed@gmail.com>
2023-11-09 13:16:16 +0000

    making the attachments for the  delete api for the attachment of the course when user what to delete a course created at the attachment sessions

---
+++ app/api/courses/[courseId]/attachments/[attachmentId]/route.ts
@@ -0,0 +1,37 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function DELETE(
+	req: Request,
+	{ params }: { params: { courseId: string; attachmentId: string } }
+) {
+	try {
+		const { userId } = auth();
+		if (!userId) {
+			return new NextResponse("Unauthorized", { status: 401 });
+		}
+
+		const ownCourse = await db.course.findUnique({
+			where: {
+				id: params.courseId,
+				userId,
+			},
+		});
+
+		if (!ownCourse) {
+			return new NextResponse("Unauthorized", { status: 401 });
+		}
+
+		const attachment = await db.attachment.delete({
+			where: {
+				id: params.attachmentId,
+				courseId: params.courseId,
+			},
+		});
+		return NextResponse.json(attachment);
+	} catch (error) {
+		console.log("[COURSE_ID] [ATTACHMENTS] [DELETE] [ERROR]", error);
+		return new NextResponse("Internal Server Error", { status: 500 });
+	}
+}
