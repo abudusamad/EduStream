4ef25a77223c6039b7b67c0c416d2f98c0d4a747
abudusamad <abudusamed@gmail.com>
2023-11-09 16:03:12 +0000

    making the api route for the unpulished course to the database

---
+++ app/api/courses/[courseId]/unpublish/route.ts
@@ -0,0 +1,40 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+	req: Request,
+	{ params }: { params: { courseId: string } }
+) {
+	try {
+		const { userId } = auth();
+
+		if (!userId) {
+			return new NextResponse("Unauthorized", { status: 401 });
+		}
+		const ownCourse = await db.course.findUnique({
+			where: {
+				id: params.courseId,
+				userId,
+			},
+		});
+		if (!ownCourse) {
+			return new NextResponse("Unauthorized", { status: 401 });
+		}
+
+		const unpublishedCourse = await db.course.update({
+			where: {
+				id: params.courseId,
+				userId,
+			},
+			data: {
+				isPublished: false,
+			},
+		});
+
+		return NextResponse.json(unpublishedCourse);
+	} catch (error) {
+		console.log("[COURSE_ID] [UNPUBLISH] [PATCH] [ERROR]", error);
+		return new NextResponse("Internal Server Error", { status: 500 });
+	}
+}
