df4c7e30fd1a01d39da98e6476b721febd9d0ea3
abudusamad <abudusamed@gmail.com>
2023-11-09 15:46:40 +0000

    making the banner

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,5 +1,3 @@
-import { IconBadge } from "@/components/icon-badge";
-import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import {
 	CircleDollarSign,
@@ -8,19 +6,27 @@ import {
 	ListChecks,
 } from "lucide-react";
 import { redirect } from "next/navigation";
+
+import { Banner } from "@/components/banner";
+import { IconBadge } from "@/components/icon-badge";
+import { db } from "@/lib/db";
+
+import { ActionForm } from "./_components/actions";
 import { AttachmentForm } from "./_components/attachment-form";
 import { CategoryForm } from "./_components/category-form";
-import { TitleForm } from "./_components/course-title";
+import { ChaptersForm } from "./_components/chapters-form";
 import { DescriptionForm } from "./_components/description-form";
 import { ImageForm } from "./_components/image-form";
 import { PriceForm } from "./_components/price-form";
-import { ChaptersForm } from "./_components/chapters-form";
+import { TitleForm } from "./_components/course-title";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
 	const { userId } = auth();
+
 	if (!userId) {
 		return redirect("/");
 	}
+
 	const course = await db.course.findUnique({
 		where: {
 			id: params.courseId,
@@ -45,78 +51,90 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
 			name: "asc",
 		},
 	});
+
 	if (!course) {
 		return redirect("/");
 	}
+
 	const requiredFields = [
 		course.title,
 		course.description,
+		course.imageUrl,
 		course.price,
 		course.categoryId,
-		course.imageUrl,
 		course.chapters.some((chapter) => chapter.isPublished),
 	];
 
 	const totalFields = requiredFields.length;
 	const completedFields = requiredFields.filter(Boolean).length;
 
-	const completionText = `${completedFields}/${totalFields}`;
+	const completionText = `(${completedFields}/${totalFields})`;
+
 	const isComplete = requiredFields.every(Boolean);
 
 	return (
-		<div className="p-6">
-			<div className="flex items-center justify-between">
-				<div className="flex flex-col gap-y-2">
-					<h1 className="text-2xl font-medium">Course setup</h1>
-					<span className="text-sm text-slate-700">
-						Complete all fields {completionText}
-					</span>
-				</div>
-			</div>
-			<div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
-				<div>
-					<div className="flex items-center gap-x-2">
-						<IconBadge icon={LayoutDashboard} />
-						<h2 className="text-xl">Customize your course</h2>
+		<>
+			{!course.isPublished && (
+				<Banner label="This course is unpublished. It will not be visible to the students." />
+			)}
+			<div className="p-6">
+				<div className="flex items-center justify-between">
+					<div className="flex flex-col gap-y-2">
+						<h1 className="text-2xl font-medium">Course setup</h1>
+						<span className="text-sm text-slate-700">
+							Complete all fields {completionText}
+						</span>
 					</div>
-					<TitleForm initialData={course} courseId={course.id} />
-					<DescriptionForm initialData={course} courseId={course.id} />
-					<ImageForm initialData={course} courseId={course.id
-					} />
-					<CategoryForm
-						initialData={course}
-						courseId={course.id}
-						options={categories.map((category) => ({
-							label: category.name,
-							value: category.id,
-						}))}
+					<ActionForm
+						disabled={!isComplete}
+						courseId={params.courseId}
+						isPublished={course.isPublished}
 					/>
 				</div>
-				<div className="space-y-6">
+				<div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
 					<div>
 						<div className="flex items-center gap-x-2">
-							<IconBadge icon={ListChecks} />
-							<h2 className="text-xl">Course chapters</h2>
+							<IconBadge icon={LayoutDashboard} />
+							<h2 className="text-xl">Customize your course</h2>
 						</div>
-						<ChaptersForm initialData={course} courseId={course.id} />
+						<TitleForm initialData={course} courseId={course.id} />
+						<DescriptionForm initialData={course} courseId={course.id} />
+						<ImageForm initialData={course} courseId={course.id} />
+						<CategoryForm
+							initialData={course}
+							courseId={course.id}
+							options={categories.map((category) => ({
+								label: category.name,
+								value: category.id,
+							}))}
+						/>
 					</div>
-					<div>
-						<div className="flex items-center gap-x-2">
-							<IconBadge icon={CircleDollarSign} />
-							<h2 className="text-xl">Sell your course</h2>
+					<div className="space-y-6">
+						<div>
+							<div className="flex items-center gap-x-2">
+								<IconBadge icon={ListChecks} />
+								<h2 className="text-xl">Course chapters</h2>
+							</div>
+							<ChaptersForm initialData={course} courseId={course.id} />
 						</div>
-						<PriceForm initialData={course} courseId={course.id} />
-					</div>
-					<div>
-						<div className="flex items-center gap-x-2">
-							<IconBadge icon={File} />
-							<h2 className="text-xl">Resources & Attachments</h2>
+						<div>
+							<div className="flex items-center gap-x-2">
+								<IconBadge icon={CircleDollarSign} />
+								<h2 className="text-xl">Sell your course</h2>
+							</div>
+							<PriceForm initialData={course} courseId={course.id} />
 						</div>
-						<AttachmentForm initialData={course} courseId={course.id} />
+						<div>
+							<div className="flex items-center gap-x-2">
+								<IconBadge icon={File} />
+								<h2 className="text-xl">Resources & Attachments</h2>
+							</div>
+							<AttachmentForm initialData={course} courseId={course.id} />
+						</div>
 					</div>
-				</div>	
+				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
