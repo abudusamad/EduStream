405e03e5373aa95c9dd46a898f04e122c4bcbe4e
abudusamad <abudusamed@gmail.com>
2023-11-10 14:51:06 +0000

    Finished with the datatable for the course and how to integrate for various areas

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -11,8 +11,8 @@ import { ChapterTitleForm } from "./_components/chapter-title-form";
 import { ChapterDescriptionForm } from "./_components/chapter-description-form";
 import { ChapterAccessForm } from "./_components/chapter-access-form";
 import { ChapterVideoForm } from "./_components/chapter-video-form";
-import { ChapterActionsForm } from "./_components/chapter-actions";
 import { Banner } from "@/components/banner";
+import { ChapterActions } from "./_components/chapter-actions";
 
 const ChapterIdPage = async ({
 	params,
@@ -73,7 +73,7 @@ const ChapterIdPage = async ({
 									Complete all fields {completionText}
 								</span>
 							</div>
-							<ChapterActionsForm
+							<ChapterActions
 								disabled={!isComplete}
 								courseId={params.courseId}
 								chapterId={params.chapterId}
