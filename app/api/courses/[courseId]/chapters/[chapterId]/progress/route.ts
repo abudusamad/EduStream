db801a8d01dd3129e52fa374c432c88ddca5f9a8
abudusamad <abudusamed@gmail.com>
2023-11-14 22:58:48 +0000

    making the api for the backend of the course

---
+++ app/api/courses/[courseId]/chapters/[chapterId]/progress/route.ts
@@ -0,0 +1,37 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+
+export async function PUT(req: Request, { params }: { params: { courseId: string; chapterId: string } }) {
+    try {
+        const { userId } = auth();
+
+        const {isCompleted} = await req.json();
+        if (!userId) {
+            return new NextResponse("Unauthorized", { status: 401 });
+        }
+
+        const userProgress = await db.userProgress.upsert({
+            where: {
+                userId_chapterId: {
+                    userId,
+                    chapterId: params.chapterId,
+                },
+            },
+            update: {
+                isCompleted,
+            },
+            create: {
+                isCompleted,
+                userId,
+                chapterId: params.chapterId,
+            },
+        })
+        return NextResponse.json(userProgress);
+        
+    } catch (error) {
+        console.log("[COURSE_CHAPTERS_PROGRESS_PUT_ERROR]", error);
+        return new NextResponse("Internal Server Error", { status: 500 });
+    }
+}
