db06cf720778db9e07491e7dcd4a225231e7d88d
abudusamad <abudusamed@gmail.com>
2023-11-14 22:44:26 +0000

    finished with the student cours and api

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -5,6 +5,9 @@ import { redirect } from "next/navigation";
 import { CourseEnrollButton } from "./_components/course-enroll-button";
 import { VideoPlayer } from "./_components/video-player";
 import { CourseProgressButton } from "./_components/course-progress-button";
+import { Separator } from "@/components/ui/separator";
+import { Preview } from "@/components/preview";
+import { File } from "lucide-react";
 
 const ChapterIdPage = async ({
 	params,
@@ -84,6 +87,28 @@ const ChapterIdPage = async ({
 							/>
 						)}
 					</div>
+					<Separator />
+					<div>
+						<Preview value={chapter.description!} />
+					</div>
+					{!!attachments.length && (
+						<>
+							<Separator />
+							<div className="p-4">
+								{attachments.map((attachment) => (
+									<a
+										href={attachment.url}
+										target="_blank"
+										key={attachment.id}
+										className="flex items-center p-3 w-full bg-sky-200 border text-sky-700 rounded-md hover:underline"
+									>
+										<File />
+										<p className="line-clamp-1">{attachment.name}</p>
+									</a>
+								))}
+							</div>
+						</>
+					)}
 				</div>
 			</div>
 		</div>
