ca0cc304350f866f84c0ef12281b45aaa8d03eaa
abudusamad <abudusamed@gmail.com>
2023-11-14 15:00:47 +0000

    makin the course sidebar

--- app/(course)/courses/[courseId]/_components/course-sidebar.tsx
+++ app/(course)/courses/[courseId]/_components/course-sidebar.tsx
@@ -1,5 +1,8 @@
+import { CourseProgress } from "@/components/course-progess";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
 import { Course ,Chapter, UserProgress} from "@prisma/client";
-
+import { redirect } from "next/navigation";
 interface CourseSidebarProps {
     course: Course& {
         chapters: (Chapter & {
@@ -10,13 +13,54 @@ interface CourseSidebarProps {
 }
 
 
-export const CourseSidebar = ({
+export const CourseSidebar = async ({
     course,
     progressCount
-}:CourseSidebarProps) => {
+}: CourseSidebarProps) => {
+    const { userId } = auth();
+    if(!userId) {
+        return redirect("/")
+    }
+
+    const purchase = await db.purchase.findUnique({
+        where: {
+            userId_courseId: {
+                userId,
+                courseId: course.id,
+            }
+        }
+    })
     return ( 
-        <div>
-            Course Sidebar
+        <div className="h-full border-r flex flex-col overflow-y-auto shadow-sm ">
+            <div className=" p-8  flex flex-col border-b">
+                <h1 className="font-semibold">
+                    {course.title}
+                </h1>
+                {
+                    purchase && (
+                        <div>
+                            <CourseProgress
+                                variant="success"
+                                value={progressCount}
+                            />
+                        </div>
+                    )
+                }
+            </div>
+            <div className="flex flex-col w-full">
+                {course.chapters.map((chapter) => (
+                    <CourseSidebarItem
+                        key={chapter.id}
+                        id={chapter.id}
+                        label={chapter.title}
+                        isCompleted={!chapter.userProgress?.[0]?.isCompleted}
+                        courseId={course.id}
+                        isLocked={!chapter.isFree && !purchase}
+                    
+                    
+                    />
+                )}
+            </div>
         </div>
      );
 }
