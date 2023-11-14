4ebad56dc7960105574dc96240c0bc8a2faf9e86
abudusamad <abudusamed@gmail.com>
2023-11-14 22:29:57 +0000

    Finished with the course makin on the page

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-progress-button.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-progress-button.tsx
@@ -1,5 +1,13 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { useConfettiStore } from "@/hooks/use-confetti-store";
+import axios from "axios";
+import { CheckCircle, XCircle } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import toast from "react-hot-toast";
+
 interface CourseProgressButtonProps {
     chapterId: string;
     courseId: string;
@@ -15,10 +23,45 @@ export const CourseProgressButton = ({
     isCompleted,
     nextChapterId
 }: CourseProgressButtonProps) => {
+
+    const router = useRouter();
+    const confetti = useConfettiStore();
+    const [isLoading, setIsLoading] = useState(false);
+
+
+    const onClick = async () => {
+        try {
+            setIsLoading(true);
+            await axios.put(`/api/courses/${courseId}/chapters/${chapterId}/progress`, { isCompleted: !isCompleted });
+            if(!isCompleted && !nextChapterId) {
+                confetti.onOpen();
+            }
+            if(!isCompleted && !nextChapterId) {
+                router.push(`/courses/${courseId}/chapters/${nextChapterId}`);
+            }
+
+            toast.success("Progress updated");
+            router.refresh();
+            
+        } catch {
+            toast.error("Something went wrong");
+        }
+        finally {
+            setIsLoading(false);
+        }
+    }
+    const Icon = isCompleted ? XCircle : CheckCircle;
     return (
-        <div>
-            Course Progress Button
-        </div>
+        <Button
+            onClick={onClick}
+            disabled={isLoading}
+            type="button"
+            variant={isCompleted ? "outline" : "success"}
+            className="w-full md:w-auto"
+        >
+            {isCompleted ? "Not completed" : "Mark as Completed"}
+            <Icon className="h-4 w-4 ml-2"/>
+       </Button>
     )
 
 }
