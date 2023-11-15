f17c294f038e5f0a76553d4349b831fd897e97d2
abudusamad <abudusamed@gmail.com>
2023-11-15 10:36:17 +0000

    making the analytics of the database site client

--- app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -1,4 +1,12 @@
-const AnalyticPage = () => {
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+const AnalyticPage = async () => {
+
+    const { userId } = auth();
+    if (!userId) {
+        return redirect("/");
+    }
     return (<div className="p-6">
         Analytics Page
     </div> );
