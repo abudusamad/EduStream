db8522991b4980f651a3e946fee24c3c184ed9e3
abudusamad <abudusamed@gmail.com>
2023-11-15 11:08:46 +0000

    Finished with the analytics of the course

--- app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -2,6 +2,7 @@ import { getAnalytics } from "@/actions/get-analytics";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import { DataCard } from "./_components/data-card";
+import { Chart } from "./_components/chat";
 
 const AnalyticPage = async () => {
 
@@ -17,7 +18,7 @@ const AnalyticPage = async () => {
 
 
     return (<div className="p-6">
-        <div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
             <DataCard
                 
                 value={totalRevenue}
@@ -25,7 +26,12 @@ const AnalyticPage = async () => {
                 shouldFormat
             
             />
+            <DataCard
+                label="Total Sales"
+                value={totalSales}
+            />
         </div>
+        <Chart data={data} />
 
     </div>);
 }
