a4d19325463eabc8066637926b8725f050139d5a
abudusamad <abudusamed@gmail.com>
2023-11-15 13:06:16 +0000

    workin on the webhook for the  stripe

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
@@ -6,43 +6,40 @@ import toast from "react-hot-toast";
 
 import { Button } from "@/components/ui/button";
 import { formatPrice } from "@/lib/format";
+
 interface CourseEnrollButtonProps {
-    price: number;
-    courseId: string;
+	price: number;
+	courseId: string;
 }
-
-
 
 export const CourseEnrollButton = ({
-    price,
-    courseId
+	price,
+	courseId,
 }: CourseEnrollButtonProps) => {
-    const [isLoading, setIsLoading] = useState(false);
-    const onClick = async () => {
-        try {
-            setIsLoading(true);
+	const [isLoading, setIsLoading] = useState(false);
 
-            const response = await axios.post(`/api/courses/${courseId}/checkout`);
-            window.location.assign(response.data.url);
-        
-        } catch {
-            toast.error("Something went wrong");
-            
-        } finally {
-            setIsLoading(false);
-        
-        }
-    }
-    return (
-        <Button
-            onClick={onClick}
-            disabled={isLoading}
-            size="sm"
-            className="w-full md:w-auto"
-            
-         >
-            Enroll for {formatPrice(price)}
+	const onClick = async () => {
+		try {
+			setIsLoading(true);
 
-        </Button>
-    )
-}
+			const response = await axios.post(`/api/courses/${courseId}/checkout`);
+
+			window.location.assign(response.data.url);
+		} catch {
+			toast.error("Something went wrong");
+		} finally {
+			setIsLoading(false);
+		}
+	};
+
+	return (
+		<Button
+			onClick={onClick}
+			disabled={isLoading}
+			size="sm"
+			className="w-full md:w-auto"
+		>
+			Enroll for {formatPrice(price)}
+		</Button>
+	);
+};
