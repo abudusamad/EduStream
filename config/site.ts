19d7a066e1011c3272dbadaea5d5045da635778d
abudusamad <abudusamed@gmail.com>
2023-12-16 10:02:40 +0000

    making the icons for the public routes and favicon

---
+++ config/site.ts
@@ -0,0 +1,4 @@
+export const siteConfig = {
+    name: "Access Learning Center",
+    description: "The Access Learning Center is a place where you can learn about the Access platform and how to use it.",
+}
