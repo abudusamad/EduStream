955b4773d5dd58fe4eb753d67aca02ee8dc68b68
abudusamad <abudusamed@gmail.com>
2023-12-27 00:24:54 +0000

    Making the mobileSidebar dynamic for different screen size and changes

--- app/(dashboard)/_components/mobile-sidebar.tsx
+++ app/(dashboard)/_components/mobile-sidebar.tsx
@@ -1,17 +1,47 @@
+"use client";
+
+import { useMedia } from "react-use";
+
 import { Menu } from "lucide-react";
 
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent } from "@/components/ui/sheet";
+import { useMobileSidebarStore } from "@/hooks/use-mobile-sidebar";
+import { useEffect, useState } from "react";
 import Sidebar from "./sidebar";
 
 export const MobileSidebar = () => {
+	const [isMouting, setIsMounting] = useState(false);
+	const isOpen = useMobileSidebarStore((state) => state.isOpen);
+	const onClose = useMobileSidebarStore((state) => state.onClose);
+	const onOpen = useMobileSidebarStore((state) => state.onOpen);
+
+	const isSmallScreen = useMedia("(max-width: 768px)", false);
+
+	useEffect(() => {
+		if (!isSmallScreen && isOpen) {
+			onClose();
+		}
+	}, [isSmallScreen, isOpen, onClose]);
+
+	useEffect(() => {
+		setIsMounting(true);
+	}, []);
+
+	if (!isMouting) {
+		return null;
+	}
+
 	return (
-		<Sheet>
-			<SheetTrigger className="md:hidden pr-4 hover:opacity-75 transition">
+		<>
+			<Button onClick={onOpen} variant="ghost" className="md:hidden pr-4">
 				<Menu />
-			</SheetTrigger>
-			<SheetContent side="left" className="p-0 bg-white">
-				<Sidebar/>
-			</SheetContent>
-		</Sheet>
+			</Button>
+			<Sheet open={isOpen} onOpenChange={onClose}>
+				<SheetContent side="left" className="p-0 bg-white">
+					<Sidebar />
+				</SheetContent>
+			</Sheet>
+		</>
 	);
 };
