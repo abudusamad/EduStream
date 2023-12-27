bf69b44879edf660ff02ed4aa55581457ac802dc
abudusamad <abudusamed@gmail.com>
2023-12-27 11:15:16 +0000

    mkaing the loading state for item

--- app/(dashboard)/(routes)/search/_components/category-items.tsx
+++ app/(dashboard)/(routes)/search/_components/category-items.tsx
@@ -3,6 +3,7 @@ import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { IconType } from "react-icons";
 
 import qs from "query-string";
+import { useEffect, useState } from "react";
 
 interface CategoryItemProps {
 	label: string;
@@ -17,7 +18,9 @@ export const CategoryItem = ({
 }: CategoryItemProps) => {
 	const pathname = usePathname();
 	const router = useRouter();
-	const searchParams = useSearchParams();
+	const searchParams = useSearchParams()
+
+	const [isLoading, setIsLoading] = useState(true);
 
 	const currentCategoryId = searchParams.get("categoryId");
 	const currentTitle = searchParams.get("title");
@@ -38,6 +41,14 @@ export const CategoryItem = ({
 		router.push(url);
 	};
 
+	useEffect(() => {
+		setIsLoading(false);
+	}, []);
+
+	if (isLoading) {
+		return <CategoryItem.Skeleton />;
+	}
+
 	return (
         <button
             onClick={onClick}
@@ -52,3 +63,12 @@ export const CategoryItem = ({
 		</button>
 	);
 };
+
+CategoryItem.Skeleton =function Skeleton() {
+	return (
+		<div className="py-2 px-3 text-sm border border-slate-200 rounded-full flex items-center">
+			<div className="w-5 h-5 rounded-full bg-slate-200 animate-pulse" />
+			<div className="w-12 h-4 ml-1 rounded-full bg-slate-200 animate-pulse" />
+		</div>
+	);
+}
