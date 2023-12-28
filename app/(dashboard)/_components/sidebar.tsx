0eab8cc5be576d63b4631c798b116d3627d9a326
abudusamad <abudusamed@gmail.com>
2023-12-28 19:55:42 +0000

    making the logo rout

--- app/(dashboard)/_components/sidebar.tsx
+++ app/(dashboard)/_components/sidebar.tsx
@@ -1,11 +1,14 @@
+"use client";
 
+import { useRouter } from "next/navigation";
 import { Logo } from "./logo";
 import { SidebarRoutes } from "./sidebar-routes";
 
 const Sidebar = () => {
+	const router = useRouter();
 	return (
 		<div className="h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm">
-			<div className="py-4  pl-6 flex items-center justify-start">
+			<div className="py-4  pl-6 flex items-center justify-start cursor-pointer" onClick={()=>router.push("/")}>
 				<Logo />
 				<span className="pl-4">
 					Acess
