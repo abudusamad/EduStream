b70817739fd6cd51f6395c6064f2148611feb842
abudusamad <abudusamed@gmail.com>
2023-12-28 19:39:23 +0000

    Making the loading the state for the analytics

--- app/(dashboard)/(routes)/teacher/analytics/_components/data-card.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/_components/data-card.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { formatPrice } from "@/lib/format";
+import { useEffect, useState } from "react";
 
 interface DataCardProps {
 	value: number;
@@ -8,19 +11,43 @@ interface DataCardProps {
 }
 
 export const DataCard = ({ value, label, shouldFormat }: DataCardProps) => {
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		setIsLoading(false);
+	}, []);
+
+	if (isLoading) {
+		return <DataCard.Skeleton />;
+	}
+
 	return (
 		<Card>
 			<CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                <CardTitle className="text-sm font-medium">
-                    {label}
-                </CardTitle>
-                
-            </CardHeader>
-            <CardContent>
-                <div className="text-2xl font-bold">
-                    {shouldFormat ? formatPrice(value) : value}
-                </div>
-            </CardContent>
+				<CardTitle className="text-sm font-medium">{label}</CardTitle>
+			</CardHeader>
+			<CardContent>
+				<div className="text-2xl font-bold">
+					{shouldFormat ? formatPrice(value) : value}
+				</div>
+			</CardContent>
+		</Card>
+	);
+};
+
+DataCard.Skeleton = function SkeletonDataCard() {
+	return (
+		<Card>
+			<CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+				<CardTitle className="text-sm font-medium">
+					<div className="w-20 h-4 bg-gray-200 rounded-md animate-pulse"></div>
+				</CardTitle>
+			</CardHeader>
+			<CardContent>
+				<div className="text-2xl font-bold">
+					<div className="w-40 h-8 bg-gray-200 rounded-md animate-pulse"></div>
+				</div>
+			</CardContent>
 		</Card>
 	);
 };
