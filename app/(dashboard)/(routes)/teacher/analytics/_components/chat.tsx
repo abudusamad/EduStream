efd456570e27cc134902401079aa49b71e1b6b6c
abudusamad <abudusamed@gmail.com>
2023-12-28 19:34:15 +0000

    Making the dashboard for the data

--- app/(dashboard)/(routes)/teacher/analytics/_components/chat.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/_components/chat.tsx
@@ -1,42 +1,58 @@
-
 "use client";
 
 import { Card } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+import { useEffect, useState } from "react";
 
 import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from "recharts";
 
 interface ChartProps {
-    data: {
-        name: string;
-        total: number;
-    }[];
+	data: {
+		name: string;
+		total: number;
+	}[];
 }
 
+export const Chart = ({ data }: ChartProps) => {
+	const [isLoading, setIsLoading] = useState(true);
 
-export const Chart = ({
-    data
-}:ChartProps) => {
-    return (
-			<Card>
-				<ResponsiveContainer width="100%" height={350}>
-					<BarChart data={data}>
-						<XAxis
-							dataKey="name"
-							stroke="#888888"
-							fontSize={12}
-							tickLine={false}
-							axisLine={false}
-						/>
-						<YAxis
-							stroke="#888888"
-							fontSize={12}
-							tickLine={false}
-							axisLine={false}
-							tickFormatter={(value) => `$${value}`}
-						/>
-						<Bar dataKey="total" fill="#0369a1" radius={[4, 4, 0, 0]} />
-					</BarChart>
-				</ResponsiveContainer>
-			</Card>
-		);
-}
+	useEffect(() => {
+		setIsLoading(false);
+	}, []);
+
+	if (isLoading) {
+		return <Chart.Skeleton />;
+	}
+	return (
+		<Card>
+			<ResponsiveContainer width="100%" height={350}>
+				<BarChart data={data}>
+					<XAxis
+						dataKey="name"
+						stroke="#888888"
+						fontSize={12}
+						tickLine={false}
+						axisLine={false}
+					/>
+					<YAxis
+						stroke="#888888"
+						fontSize={12}
+						tickLine={false}
+						axisLine={false}
+						tickFormatter={(value) => `$${value}`}
+					/>
+					<Bar dataKey="total" fill="#0369a1" radius={[4, 4, 0, 0]} />
+				</BarChart>
+			</ResponsiveContainer>
+		</Card>
+	);
+};
+
+Chart.Skeleton = function SkeletonChart() {
+	return (
+		<Card>
+			<Skeleton className="w-full h-[350px] animate-ring" />
+		</Card>
+	);
+};
+
