195add5ad503b3fe102ef80425fa0e3ee9cb3d24
abudusamad <abudusamed@gmail.com>
2024-02-13 21:37:11 +0000

    Making the landing page for the site

--- app/(landing)/_components/heading.tsx
+++ app/(landing)/_components/heading.tsx
@@ -1,9 +1,13 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { useClerk, useSignIn } from "@clerk/nextjs";
 
 export const Heading = () => {
+	const clerk = useClerk();
 	return (
 		<div className="relative h-full w-full bg-no-repeat bg-cover">
-			<div
+			{/* <div
 				className=" absolute inset-0 bg-cover"
 				style={{
 					backgroundImage:
@@ -11,8 +15,8 @@ export const Heading = () => {
 
 					opacity: 0.1,
 				}}
-			/>
-			<div className="md:pl-20  md:grid grid-cols-3 md:max-w-7xl pt-40  ">
+			/> */}
+			<div className="md:pl-20  md:grid grid-cols-3 md:max-w-7xl pt-40 ">
 				<div className="flex flex-col col-span-2 space-y-9">
 					<h1 className="text-xl sm:text-5xl md:text-6xl font-bold uppercase ">
 						Efficent school, learning & school management
@@ -32,16 +36,43 @@ export const Heading = () => {
 						Join us and discover new levels of efficiency.
 					</p>
 					<div className="flex space-x-4">
-						<Button size="lg" className="bg-rose-500 text-white text-3xl font-bold p-4 opacity-100">
+						<Button
+							size="lg"
+							className="bg-rose-700 text-white text-lg font-semibold p-4 opacity-100 hover:none"
+						>
 							Talk to An Expertise
 						</Button>
-						<Button size="lg"  variant="outline" className="border-2 outline-4" >
-						Login
+						<Button
+							size="lg"
+							variant="outline"
+							className="border-2 outline-4 cursor-pointer"
+							onClick={() => clerk.openSignIn({})}
+						>
+							Login
 						</Button>
 					</div>
 				</div>
 				<p className="text-lg">The best way to manage your projects</p>
 			</div>
+
+			<svg
+				className="hidden dark:block"
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 1440 320"
+			>
+				<path
+					fill="#ff0000"
+					fill-opacity="10"
+					d="M0,128L80,122.7C160,117,320,107,480,85.3C640,64,800,32,960,21.3C1120,11,1280,21,1360,26.7L1440,32L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+				></path>
+			</svg>
+			<svg className="dark:hidden"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+				<path
+					fill="#0099ff"
+					fill-opacity="10"
+					d="M0,128L80,122.7C160,117,320,107,480,85.3C640,64,800,32,960,21.3C1120,11,1280,21,1360,26.7L1440,32L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
+				></path>
+			</svg>
 		</div>
 	);
 };
