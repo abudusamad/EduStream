25fe1e5142f8ee5d761daf85d56423e296d69868
abudusamad <abudusamed@gmail.com>
2024-02-13 00:29:35 +0000

    finished with making the site

--- app/(landing)/_components/heading.tsx
+++ app/(landing)/_components/heading.tsx
@@ -1,8 +1,35 @@
 export const Heading = () => {
 	return (
-		<div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
-			<h1 className="text-4xl font-bold">Welcome to Acess</h1>
-			<p className="text-lg">The best way to manage your projects</p>
+		<div className="relative h-full w-full bg-no-repeat bg-cover">
+			<div className="max-w-3xl md:grid grid-cols-2 md:max-w-7xl pt-40  ">
+				<div
+					className=" absolute inset-0 bg-cover"
+					style={{
+						backgroundImage:
+							"url(https://cdn.elearningindustry.com/wp-content/uploads/2017/03/6-elearning-strategies-to-develop-deeper-learning-skills-e1489405475782.jpeg)",
+
+						opacity: 0.1,
+					}}
+				/>
+				<div className="flex flex-col space-y-9">
+					<h1 className="text-3xl sm:text-5xl md:text-6xl font-bold uppercase">
+						Efficent school, learning & school management
+						<span className="font-bold text-rose-700"> Solution</span>
+					</h1>
+					<h3 className="text-2xl font-semibold md:text-3xl">
+						Ensuring Excellence and Compliance in Your Educational Environment
+					</h3>
+					<p className="text-2xl font-light">
+						Our mission is to help educational institutions digitize and
+						simplify all processes from enrollment to alumni. Embrace a new
+						world of efficiency where back office, educators, students, and
+						parents converge seamlessly, streamlining communications,
+						administration, operations, and academics in a single, dynamic hub.
+						Join us and discover new levels of efficiency.
+					</p>
+				</div>
+				<p className="text-lg">The best way to manage your projects</p>d
+			</div>
 		</div>
 	);
 };
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -3,7 +3,7 @@ import { Heading } from "./_components/heading";
 const LandingPage = () => {
 	return (
 		<div className="min-h-full flex flex-col ">
-			<div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+			<div className="flex flex-col items-center justify-center md:justify-start gap-y-8 flex-1 px-6 pb-10 ">
 				<Heading />
 			</div>
 		</div>
