eb4b3a8e244281c235497537d74b08fb06cdf8a2
abudusamad <abudusamed@gmail.com>
2024-02-13 23:56:22 +0000

    Adding spiner for the loading page

--- components/spinner.tsx
+++ components/spinner.tsx
@@ -1,30 +1,25 @@
 import { cn } from "@/lib/utils";
-import { type VariantProps, cva } from "class-variance-authority"
-import { Loader } from "lucide-react"
+import { cva, type VariantProps } from "class-variance-authority";
+import { Loader } from "lucide-react";
 
-const spinnerVariants = cva(
-    "text-muted-foreground animate-spin",
-    {
-        variant: {
-            size: {
-                default: "w-6 h-6",
-                sm: "w-4 h-4",
-                md: "w-8 h-8",
-                lg: "w-12 h-12",
-                xl: "w-16 h-16",
-                icon: "w-6 h-6",
-            }
-        }, defaultVarints: {
-            size: "default"
-        }
-    }
-);
+const spinnerVariants = cva("text-muted-foreground animate-spin", {
+	variants: {
+		size: {
+			default: "w-6 h-6",
+			sm: "w-4 h-4",
+			md: "w-8 h-8",
+			lg: "w-12 h-12",
+			xl: "w-16 h-16",
+			icon: "w-6 h-6",
+		},
+	},
+	defaultVariants: {
+		size: "default",
+	},
+});
 
-interface SpinnerProps  extends VariantProps<typeof spinnerVariants> {}
+interface SpinnerProps extends VariantProps<typeof spinnerVariants> {}
 
-export const Spinner = ({size}:SpinnerProps) => {
-    return (
-        <Loader className={cn(spinnerVariants({ size }))} />
-    
-    )
-}
+export const Spinner = ({ size }: SpinnerProps) => {
+	return <Loader className={cn(spinnerVariants({ size }))} />;
+};
