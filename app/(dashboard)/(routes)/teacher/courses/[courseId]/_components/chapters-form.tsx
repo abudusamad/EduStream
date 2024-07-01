9dedf69f383a793b0947afc31b97c7258737606c
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-07-01 16:15:02 +0000

    Making the pages for the course

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
@@ -20,8 +20,8 @@ import {
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { Input } from "@/components/ui/input";
-
 import { ChaptersList } from "./chapters-list";
+
 
 interface ChaptersFormProps {
 	initialData: Course & { chapters: Chapter[] };
