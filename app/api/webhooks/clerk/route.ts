6bff9451a485aca7e4fc5aeb6324aded6cd73d21
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-07-11 22:09:07 +0000

    Updating the clerk webhook to update external_userId

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,6 +1,7 @@
 import { Webhook } from 'svix'
 import { headers } from 'next/headers'
 import { WebhookEvent } from '@clerk/nextjs/server'
+import { db } from '@/lib/db'
 
 export async function POST(req: Request) {
 
@@ -47,12 +48,41 @@ export async function POST(req: Request) {
     })
   }
 
-  // Do something with the payload
-  // For this guide, you simply log the payload to the console
-  const { id } = evt.data;
   const eventType = evt.type;
-  console.log(`Webhook with and ID of ${id} and type of ${eventType}`)
-  console.log('Webhook body:', body)
+ 
+  if (eventType === 'user.created') {
+    await db.user.create({
+      data: {
+        externalUserId: payload.user.id,
+        username: payload.user.username,
+        imageUrl: payload.user.imageUrl,
 
+      }
+    })
+  }
+
+  //Update User in DB
+
+  if (eventType === 'user.updated') {
+    await db.user.update({
+      where: {
+        externalUserId: payload.user.id
+      },
+      data: {
+        username: payload.user.username,
+        imageUrl: payload.user.imageUrl,
+      }
+    })
+  }
+
+  //Delete User in DB
+
+  if (eventType === 'user.deleted') {
+    await db.user.delete({
+      where: {
+        externalUserId: payload.user.id
+      }
+    })
+  }
   return new Response('', { status: 200 })
 }
