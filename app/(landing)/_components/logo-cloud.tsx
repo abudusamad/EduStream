0b9fa6c5dac1ea4c9c4ad640d9a595839b5e7b89
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-07-15 23:42:21 +0000

    adding the prisma-migrate

--- app/(landing)/_components/logo-cloud.tsx
+++ app/(landing)/_components/logo-cloud.tsx
@@ -5,11 +5,11 @@ import "./style/logoCloud.css"
 export function LogoCloud() {
   return (
     <div className="logo-cloud-container mx-auto mt-16 max-w-7xl px-6 lg:px-8">
-      <h2 className="text-center font-cal text-lg leading-8 text-gray-900">
+      <h2 className="text-center font-cal text-lg leading-8 text-gray-900 dark:text-gray-200" >
         Trusted by 5000+ happy users
       </h2>
       <div className="scrolling-wrapper">
-        <div className="image-container mx-auto mt-8 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-12 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 sm:gap-y-14 lg:mx-0 lg:max-w-none lg:grid-cols-5">
+        <div className="image-container mx-auto mt-8 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-12 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 sm:gap-y-14 lg:mx-0 lg:max-w-none lg:grid-cols-5 ">
           {[...Array(2)].map((_, index) => (
             <React.Fragment key={index}>
               <Image
---
+++ prisma/migrations/20240715233643_adding_user_model/migration.sql
@@ -0,0 +1,152 @@
+-- CreateTable
+CREATE TABLE "Course" (
+    "id" STRING NOT NULL,
+    "userId" STRING NOT NULL,
+    "title" STRING NOT NULL,
+    "description" STRING,
+    "imageUrl" STRING,
+    "price" FLOAT8,
+    "isPublished" BOOL NOT NULL DEFAULT false,
+    "categoryId" STRING,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Course_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" STRING NOT NULL,
+    "name" STRING NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Attachment" (
+    "id" STRING NOT NULL,
+    "name" STRING NOT NULL,
+    "url" STRING NOT NULL,
+    "courseId" STRING NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Attachment_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Chapter" (
+    "id" STRING NOT NULL,
+    "title" STRING NOT NULL,
+    "description" STRING,
+    "videoUrl" STRING,
+    "position" INT4 NOT NULL,
+    "isPublished" BOOL NOT NULL DEFAULT false,
+    "isFree" BOOL NOT NULL DEFAULT false,
+    "courseId" STRING NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Chapter_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "MuxData" (
+    "id" STRING NOT NULL,
+    "assetId" STRING NOT NULL,
+    "playbackId" STRING,
+    "chapterId" STRING NOT NULL,
+
+    CONSTRAINT "MuxData_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UserProgress" (
+    "id" STRING NOT NULL,
+    "userId" STRING NOT NULL,
+    "chapterId" STRING NOT NULL,
+    "isCompleted" BOOL NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "UserProgress_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Purchase" (
+    "id" STRING NOT NULL,
+    "userId" STRING NOT NULL,
+    "courseId" STRING NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Purchase_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "StripeCustomer" (
+    "id" STRING NOT NULL,
+    "userId" STRING NOT NULL,
+    "stripeCustomerId" STRING NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "StripeCustomer_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Course_userId_key" ON "Course"("userId");
+
+-- CreateIndex
+CREATE INDEX "Course_categoryId_idx" ON "Course"("categoryId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Course_title_key" ON "Course"("title");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_name_key" ON "Category"("name");
+
+-- CreateIndex
+CREATE INDEX "Attachment_courseId_idx" ON "Attachment"("courseId");
+
+-- CreateIndex
+CREATE INDEX "Chapter_courseId_idx" ON "Chapter"("courseId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "MuxData_chapterId_key" ON "MuxData"("chapterId");
+
+-- CreateIndex
+CREATE INDEX "UserProgress_chapterId_idx" ON "UserProgress"("chapterId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserProgress_userId_chapterId_key" ON "UserProgress"("userId", "chapterId");
+
+-- CreateIndex
+CREATE INDEX "Purchase_courseId_idx" ON "Purchase"("courseId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Purchase_userId_courseId_key" ON "Purchase"("userId", "courseId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "StripeCustomer_userId_key" ON "StripeCustomer"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "StripeCustomer_stripeCustomerId_key" ON "StripeCustomer"("stripeCustomerId");
+
+-- AddForeignKey
+ALTER TABLE "Course" ADD CONSTRAINT "Course_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Attachment" ADD CONSTRAINT "Attachment_courseId_fkey" FOREIGN KEY ("courseId") REFERENCES "Course"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Chapter" ADD CONSTRAINT "Chapter_courseId_fkey" FOREIGN KEY ("courseId") REFERENCES "Course"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "MuxData" ADD CONSTRAINT "MuxData_chapterId_fkey" FOREIGN KEY ("chapterId") REFERENCES "Chapter"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "UserProgress" ADD CONSTRAINT "UserProgress_chapterId_fkey" FOREIGN KEY ("chapterId") REFERENCES "Chapter"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Purchase" ADD CONSTRAINT "Purchase_courseId_fkey" FOREIGN KEY ("courseId") REFERENCES "Course"("id") ON DELETE CASCADE ON UPDATE CASCADE;
