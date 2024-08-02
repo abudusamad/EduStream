961be8e6a1d9be0ae95f077e8dcb3c40e69851d7
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-08-02 23:05:31 +0000

    Makin the page

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -22,6 +22,6 @@ export async function POST(req: Request) {
 		return NextResponse.json(course);
 	} catch (error) {
 		console.log("[COURSES]", error);
-		return new NextResponse("Internal Error", { status: 500 });
+		return new NextResponse("Internal Server Error", { status: 500 });
 	}
 }
