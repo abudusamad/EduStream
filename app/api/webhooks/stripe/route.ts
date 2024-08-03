df761ed65a0b72c90ea9484cf309cbf250c0beae
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-08-03 00:23:34 +0000

    Making the checkout page

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -6,44 +6,39 @@ import { stripe } from "@/lib/stripe";
 import { db } from "@/lib/db";
 
 export async function POST(req: Request) {
-	const body = await req.text();
-	const signature = headers().get("Stripe-Signature") as string;
+  const body = await req.text();
+  const signature = headers().get("Stripe-Signature") as string;
 
-	let event: Stripe.Event;
+  let event: Stripe.Event;
 
-	try {
-		event = stripe.webhooks.constructEvent(
-			body,
-			signature,
-			process.env.STRIPE_WEBHOOK_SECRET!
-		);
-	} catch (error: any) {
-		return new NextResponse(`Webhook Error: ${error.message}`, { status: 400 });
-	}
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET!
+    )
+  } catch (error: any) {
+    return new NextResponse(`Webhook Error: ${error.message}`, { status: 400 })
+  }
 
-	const session = event.data.object as Stripe.Checkout.Session;
-	const userId = session?.metadata?.userId;
-	const courseId = session?.metadata?.courseId;
+  const session = event.data.object as Stripe.Checkout.Session;
+  const userId = session?.metadata?.userId;
+  const courseId = session?.metadata?.courseId;
 
-	if (event.type === "checkout.session.completed") {
-		if (!userId || !courseId) {
-			return new NextResponse(`Webhook Error: Missing metadata`, {
-				status: 400,
-			});
-		}
+  if (event.type === "checkout.session.completed") {
+    if (!userId || !courseId) {
+      return new NextResponse(`Webhook Error: Missing metadata`, { status: 400 });
+    }
 
-		await db.purchase.create({
-			data: {
-				courseId: courseId,
-				userId: userId,
-			},
-		});
-	} else {
-		return new NextResponse(
-			`Webhook Error: Unhandled event type ${event.type}`,
-			{ status: 200 }
-		);
-	}
+    await db.purchase.create({
+      data: {
+        courseId: courseId,
+        userId: userId,
+      }
+    });
+  } else {
+    return new NextResponse(`Webhook Error: Unhandled event type ${event.type}`, { status: 200 })
+  }
 
-	return new NextResponse(null, { status: 200 });
+  return new NextResponse(null, { status: 200 });
 }
