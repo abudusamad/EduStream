81a260c0938c4182cfc467fd1fe0bed99e380870
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-08-03 00:47:59 +0000

    making the page dynmic

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -2,16 +2,14 @@ import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
 import { db } from "@/lib/db";
+import { isTeacher } from "@/lib/teacher";
 
 export async function POST(req: Request) {
 	try {
 		const { userId } = auth();
 		const { title } = await req.json();
-		if (!title) {
-			return new NextResponse("Invalid field", {status: 401})
-		}
 
-		if (!userId ) {
+		if (!userId || !isTeacher(userId)) {
 			return new NextResponse("Unauthorized", { status: 401 });
 		}
 
@@ -23,7 +21,6 @@ export async function POST(req: Request) {
 		});
 
 		return NextResponse.json(course);
-		console.log(course)
 	} catch (error) {
 		console.log("[COURSES]", error);
 		return new NextResponse("Internal Server Error", { status: 500 });
