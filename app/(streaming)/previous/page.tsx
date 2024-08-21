06b161d5db42f3612274c43ada150cec9f768927
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-08-21 03:05:11 +0000

    Making the previous section

---
+++ app/(streaming)/previous/page.tsx
@@ -0,0 +1,14 @@
+import CallList from "../_components/CallList";
+
+
+const PreviousPage = () => {
+  return (
+    <section className="flex size-full flex-col gap-10 text-white">
+      <h1 className="text-3xl font-bold">Previous Calls</h1>
+
+      <CallList type="ended" />
+    </section>
+  );
+};
+
+export default PreviousPage;
