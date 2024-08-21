941d30a6b6cd5a152afc8dc9ab3891442ef24313
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-08-21 03:07:09 +0000

    Making the recordings sessions

---
+++ app/(streaming)/recordings/page.tsx
@@ -0,0 +1,14 @@
+import CallList from "../_components/CallList";
+
+
+const PreviousPage = () => {
+  return (
+    <section className="flex size-full flex-col gap-10 text-white">
+      <h1 className="text-3xl font-bold">Recordings</h1>
+
+      <CallList type="recordings" />
+    </section>
+  );
+};
+
+export default PreviousPage;
