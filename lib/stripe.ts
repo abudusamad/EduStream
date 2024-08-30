714f20d2fba8f1914c8b3e136342b2800c9fa509
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-08-30 11:44:16 +0000

    updating the stripe api version

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,6 +1,6 @@
 import Stripe from 'stripe';
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-    apiVersion: '2023-10-16',
+    apiVersion: "2024-06-20",
     typescript: true,
 });
